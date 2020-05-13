e5cd4eb844df48aa1a678d41858e77933ee9d11a
Arrowfield <768449566@qq.com>
2020-05-13 22:59:31 +0800

    update

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -2,84 +2,107 @@
   <div class="app-container">
     <el-form ref="form" :model="form" label-width="120px">
       <el-form-item label="文章标题">
-        <el-input v-model="form.name" />
+        <el-input v-model="form.articleTitle" />
       </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
-        </el-select>
+      <el-form-item label="文章摘要">
+        <el-input v-model="form.articleAbstract" />
       </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
-        </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
-        </el-col>
+      <el-form-item label="文章标签">
+        <el-tag class="tag" v-for="tag in tags" :key="tag.name" closable :type="tag.type">
+          {{tag.name}}
+        </el-tag>
       </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
+
+      <el-form-item label="正文">
+        <el-input type="textarea" :rows="15" placeholder="请输入内容" v-model="textarea">
+        </el-input>
       </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
+
+      <el-form-item label="文章首图">
+        <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          <!-- <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div> -->
+        </el-upload>
       </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
+
+      <el-form-item label="文章访问路径">
+        <el-input v-model="form.articleAbstract" />
+      </el-form-item>
+
+      <el-form-item label="创建时间">
+        <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="文章状态">
+        <el-radio-group v-model="radio">
+          <el-radio :label="3">备选项</el-radio>
+          <el-radio :label="6">备选项</el-radio>
+          <el-radio :label="9">备选项</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
-      </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
+        <el-button type="primary" @click="onSubmit">立即添加文章</el-button>
+        <el-button>取消</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+  export default {
+    data() {
+      return {
+        form: {
+          articleTitle: "",
+          articleAbstract: "",
+          articleTags: []
+        },
+        tags: [{
+            name: '标签一',
+            type: ''
+          },
+          {
+            name: '标签二',
+            type: 'success'
+          },
+          {
+            name: '标签三',
+            type: 'info'
+          },
+          {
+            name: '标签四',
+            type: 'warning'
+          },
+          {
+            name: '标签五',
+            type: 'danger'
+          }
+        ]
+      }
+    },
+    methods: {
+      onSubmit() {
+        this.$message('submit!')
+      },
+      onCancel() {
+        this.$message({
+          message: 'cancel!',
+          type: 'warning'
+        })
       }
     }
-  },
-  methods: {
-    onSubmit() {
-      this.$message('submit!')
-    },
-    onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
-    }
   }
-}
+
 </script>
 
 <style scoped>
-.line{
-  text-align: center;
-}
-</style>
+  .line {
+    text-align: center;
+  }
 
+  .tag {
+    margin-right: 20px;
+  }
+
+</style>
