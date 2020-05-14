4ec4c35efcfecf41eb6807541126bc339d2ce72c
v_zipfang <768449566@qq.com>
2020-05-14 14:19:57 +0800

    update

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -14,8 +14,6 @@
       </el-form-item>
 
       <el-form-item label="正文">
-<!--        <el-input type="textarea" v-model="textArea" :rows="15" placeholder="请输入内容"/>-->
-
         <tinymce :height="300" v-model="content" ></tinymce>
       </el-form-item>
 
@@ -61,9 +59,7 @@
 
     data() {
       return {
-        content:" `<h1 style=\"text-align: center;\">Welcome to the TinyMCE demo!</h1><p style=\"text-align: center; font-size: 15px;\"><img title=\"TinyMCE Logo\" src=\"//www.tinymce.com/images/glyph-tinymce@2x.png\" alt=\"TinyMCE Logo\" width=\"110\" height=\"97\" /><ul>\n" +
-          "        <li>Our <a href=\"//www.tinymce.com/docs/\">documentation</a> is a great resource for learning how to configure TinyMCE.</li><li>Have a specific question? Visit the <a href=\"https://community.tinymce.com/forum/\">Community Forum</a>.</li><li>We also offer enterprise grade support as part of <a href=\"https://tinymce.com/pricing\">TinyMCE premium subscriptions</a>.</li>\n" +
-          "      </ul>`",
+        content:"",
         form: {
           articleTitle: "",
           articleAbstract: "",
